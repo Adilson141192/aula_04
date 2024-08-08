e96033ae8439ccdf123fa1b020c422ed3762d71b
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-08 14:54:07 -0300

    080824 aula0404

--- array.js
+++ array.js
@@ -2,6 +2,7 @@ var valores = [8, 1, 7, 2, 9];
 
 console.log(valores[3]);
 
+console.log("Array invertido: " + valores.reverse());
 
 for (var pos = 0 ; pos < valores.length; pos++){
     console.log("Posicao: " + pos + " Valor " + valores[pos]);
@@ -25,3 +26,29 @@ for (var pos = 0 ; pos < valores.length; pos++){
 var media = soma/valores.length
 
 console.log(media);
+
+
+var arr1 = [1,2,3,4,5,6];
+
+console.log(arr1.join(' '));
+
+var retirado = arr1.shift();
+
+console.log(retirado);
+
+console.log(arr1);
+
+console.log("Posição do número 3: " + arr1.indexOf(3));
+
+arr1.push(7);
+
+console.log(arr1);
+
+arr1.pop();
+
+console.log(arr1);
+
+
+var ar = [-5,10,20,-3,89];
+
+
